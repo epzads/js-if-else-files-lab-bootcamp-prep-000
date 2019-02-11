ab906068845cf9ec665b24c71345fa5410718c86
favorable-vector-1743 <epzads@gmail.com>
2019-02-11 17:48:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-var name = "Joe"
-var height = "74"
+var name = "Joe";
+var height = "74";
 
 
 // Don't worry about this
 module.exports = { name, height
 }
 
+
+
+
+if (name === "susan")
+{
+  
+  console.log ("The name is correct");
+  
+}
+
+else 
+
+{
+  
+  console.log ("the name is expected to be susan");
+}
